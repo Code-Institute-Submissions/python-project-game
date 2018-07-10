d405500191b9c6b94deac84ecaeb3f4625eba2ea
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-07-10 13:38:00 +0000

    trying to get simple question and answer to work. Very messy

--- app.py
+++ app.py
@@ -1,89 +1,144 @@
 import os
 import json
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect
 
 app = Flask(__name__)
 app.secret_key = "coffee_secret"
 
-def quiz_menu():
-    """Allows the user to begin the quiz"""
-    print("1. Start Quiz!")
-    print("2. Quit Game")
+# def quiz_menu():
+#     """
+#     Allows the user to begin the quiz
+#     """
+#     print("1. Start Quiz!")
+#     print("2. Quit Game")
     
-    #Then we'll write an option variable that takes user's input
-    option = input("Enter option: ")
-    #Our function is going to return whathever option our user selects
-    return option
+#     #Then we'll write an option variable that takes user's input
+#     option = input("Enter option: ")
+#     #Our function is going to return whathever option our user selects
+#     return option
 
 
-def get_coffee_questions():
-    """
-    Gets the questions from our .json file
-    """
+# def get_coffee_questions():
+#     """
+#     Gets the questions from our .json file
+#     """
     
-    #We open our json file and name it coffee_quiz
-    with open("data/coffee_questions.json", "r") as file:
-        coffee_quiz = json.load(file)
-        # print(coffee_quiz["quiz_1"]["question"], coffee_quiz["quiz_2"]["question"])
-        # print(coffee_quiz)
-        for key, value in coffee_quiz.items():
-            print(value["question"])
-            print(value["answer"])
+#     #We open our json file and name it coffee_quiz
+#     with open("data/coffee_questions.json", "r") as file:
+#         coffee_quiz = json.load(file)
+#         # print(coffee_quiz["quiz_1"]["question"], coffee_quiz["quiz_2"]["question"])
+#         # print(coffee_quiz)
+#         for key, value in coffee_quiz.items():
+#             print(value["question"])
+#             print(value["answer"])
 
-    #Now we get the length of our list of questions, which will be needed to keep
-    #track of scores and let the user know how long is the quiz
-    number_of_coffee_questions = len(coffee_quiz)
-    print(number_of_coffee_questions)
+#     #Now we get the length of our list of questions, which will be needed to keep
+#     #track of scores and let the user know how long is the quiz
+#     number_of_coffee_questions = len(coffee_quiz)
+#     print(number_of_coffee_questions)
     
-    #At this point, we need to iterate through our json file in order to get 
-    #our questions and answers, which we'll need for scoreing and guesses below
+#     #At this point, we need to iterate through our json file in order to get 
+#     #our questions and answers, which we'll need for scoreing and guesses below
     
-    ##PENDING - GET Q & A - FOR LOOP ?
+#     ##PENDING - GET Q & A - FOR LOOP ?
     
-    #We initialise an empty variable to keep track of the score
-    score = 0
+#     #We initialise an empty variable to keep track of the score
+#     score = 0
     
-    #And we're ready to allow our user to input their guess
+#     #And we're ready to allow our user to input their guess
     
-    ##ANOTHER FOR LOOP FOR THE GUESSES OF EACH Q&A ?
+#     ##ANOTHER FOR LOOP FOR THE GUESSES OF EACH Q&A ?
       
-    return coffee_quiz
+#     return coffee_quiz
     
-def game_loop():
-    """Activates our game"""
-    while True:
-        option = quiz_menu()
-        if option == "1":
-            # print("You selected 'Start Quiz!'")
-            get_coffee_questions()
-            # ask_questions()
-        elif option == "2":
-            print("Thanks for Playing. Good Bye")
-            break
-        else:
-            print("Invalid option")
-        #After everything we'll print a blank line for aesthetic reasons
-        print("")
+# def game_loop():
+#     """Activates our game"""
+#     while True:
+#         option = quiz_menu()
+#         if option == "1":
+#             # print("You selected 'Start Quiz!'")
+#             get_coffee_questions()
+#             # ask_questions()
+#         elif option == "2":
+#             print("Thanks for Playing. Good Bye")
+#             break
+#         else:
+#             print("Invalid option")
+#         #After everything we'll print a blank line for aesthetic reasons
+#         print("")
 
 @app.route("/")
 def index():
     return render_template("index.html")
     
-# @app.route("/index/<quiz_number>")
-# def quiz(quiz_number):
-#     quiz = {}
+@app.route("/quiz", methods=["GET", "POST"])
+# We create a var with a couple of question to get it to work on a simple level
+def get_coffee_questions():
+    riddles = [
+    {"question": "short coffee?", "answer": "Espresso"},
+    {"question": "Long coffee?", "answer": "Americano"},
+    {"question": "Milky coffee?", "answer": "Latte"}
+    ] 
+    
+    first_question = riddles[0]["question"]
+    print(first_question)
+    first_answer = riddles[0]["answer"]
+    print(first_answer)
+    
+    score = 0
+    # session = 1
+    
+    if request.method == "POST":
+        if request.form["answer"].lower() == first_answer:
+            score += 1
+            flash("Correct!")
+    
+        return render_template("quiz.html", first_question=first_question)
+    
+    
+    
+    # #Logic example 
+    # if request.method == "POST" and session["riddle_num"] < len(riddles):
+    #     previous_riddle = riddles[session["riddle_num"]]
+    #     if request.form["answer"].lower() == previous_riddle["answer"]:
+    #         session["riddle_num"] += 1
+    #         session["score"] += 1
+    #         if session["riddle_num"] < len(riddles):
+    #             flash("Correct answer, %s! Your score is %s." % (
+    #                   session["player"], session["score"]))
+        
+# @app.route("/index/<quiz_number>", methods=["GET", "POST"])
+# def get_coffee_questions(quiz_number):
+#     coffee_question = {}
     
 #     with open("data/coffee_questions.json", "r") as file:
 #         quiz = json.load(file)
+        
+#         # print(quiz["quiz_1"]["question"], quiz["quiz_2"]["question"])
+#         # print(quiz)
+#         # for key, value in quiz.items():
+#         #     print(value["question"])
+#         #     print(value["answer"])
+
+        
 #         for question in quiz:
 #             if question["url"] == quiz_number:
-#                 quiz = question
-   
-#     return render_template("quiz.html", quiz=quiz)
+#                 coffee_question = question
+                
+#     return render_template("quiz.html", coffee_question=coffee_question)
+    
 
+
+#     #Now we get the length of our list of questions, which will be needed to keep
+#     #track of scores and let the user know how long is the quiz
+#     number_of_coffee_questions = len(coffee_quiz)
+#     print(number_of_coffee_questions)
+    
 # PENDING - Changes need to be made as the program runs when testing which is 
 #unwanted. Try moving the game_loop function later
 # game_loop()
+
+# get_coffee_questions()
 
 if __name__ == "__main__":
     # game_loop()
