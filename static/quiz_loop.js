9f49fe3e04d1848597270d8381723fc960e127e3
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-08-15 09:48:30 +0000

    added modal for mistake. Need to work on its logic

--- static/quiz_loop.js
+++ static/quiz_loop.js
@@ -1,24 +1,5 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-//     $('#submit_answer').on('click', function() {
-//     preventDefault();
-//     var answer = $('#answer_input').val();
-//     var username = '{{ username }}';
-//     $.post({
-//     url: "/quiz",
-//     data: {
-//         username: username,
-//         answer: answer
-//     },
-//     success: function(data) {
-//         $('#answer_box').html(data);
-//     },
-//     traditional: true
-//     }).done();
-// });
-
-    // We need a function to cnage the CSS of the #discover-answer button when the 
-    // answer is incorrect
     
     if (coffee_guess !== coffee_answer) {
         $("#discover-answer").css("visibility", "visible");
