8f9707dd366406f8824a69a0b9f8591133ee9e1a
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-08-16 21:28:09 +0000

    quiz now doesn't give points if you look at the solution

--- app.py
+++ app.py
@@ -15,21 +15,21 @@ with open("data/coffee_questions.json", "r") as json_file:
 
 def get_coffee_questions(placeholderForRiddles, i):
     """
-    This function searches for and returns questions for the quiz 
+    This function searches for and returns questions for the quiz
     """
     placeholderQuestion = placeholderForRiddles[i]["question"]
     return placeholderQuestion
 
 def get_coffee_images(placeholderForRiddles, i):
     """
-    This function searches for and returns images for the quiz 
+    This function searches for and returns images for the quiz
     """
     placeholderImage = placeholderForRiddles[i]["image"]
     return placeholderImage
 
 def get_coffee_answers(placeholderForRiddles, i):
     """
-    This function searches for and returns answers for the quiz 
+    This function searches for and returns answers for the quiz
     """
     placeholderAnswer = placeholderForRiddles[i]["answer"]
     return placeholderAnswer
@@ -43,7 +43,7 @@ def add_top_score(username, total_score):
     with open('data/top_scores.txt', 'a+') as top_scores:
         if not (username, total_score) in top_players:
             top_scores.write('\n{}:{}'.format(str(username), str(total_score)))
-            
+
 def get_top_players():
     # you should be able to access this here: print(session['score'])
     # Append this player's score to the file with add_top_score()
@@ -51,18 +51,18 @@ def get_top_players():
         top_players = []
         for line in top_scores.readlines()[1:]:
             top_players.append(line)
-        
+
         sorted_top_players = []
         for player in top_players:
             tupe = (player.split(':')[0].strip(), int(player.split(':')[1].strip())) # tupe = (username, score)
             sorted_top_players.append(tupe)
-            
+
         # Sort top_players on the 2nd elem of the tuple, reverse the sort, then return the top 5
-        # Another way: 
+        # Another way:
         # sorted_final = sorted(sorted_top_players, key=lambda x: x[1]) # key = def function(x): return x[1]
-        
+
         return sorted(sorted_top_players, key=lambda x: x[1])[::-1][:5]
-    
+
 # Getting the top scores - END
 
 #Now we start writing our routes
@@ -76,11 +76,11 @@ def index():
         session['quiz_num'] = 0
         return redirect(url_for("get_coffee_quiz"))
     return render_template("index.html")
-    
+
 
 @app.route("/quiz", methods=["GET", "POST"])
 def get_coffee_quiz():
-    
+
     #first time page loads, sess num is 0, and takes q0 and a0 into coffee answer
     #then i post it, but it all runs again before it checks to see whether it was get or post
     #so sesion num is still zero at that point#
@@ -90,17 +90,17 @@ def get_coffee_quiz():
     #Trying to get image - START
     coffee_image = get_coffee_images(riddles, session['quiz_num'])
     #Trying to get image - END
-    coffee_answer = get_coffee_answers(riddles, session['quiz_num']).lower() # I'd actually put these in the GET request, 
+    coffee_answer = get_coffee_answers(riddles, session['quiz_num']).lower() # I'd actually put these in the GET request,
     # noting the above ^^^ and get rid of "i"
-    
+
     #Calling question and answer functions - END
-    
-    
+
+
     #Now we get the number of riddles in our quiz
     number_of_questions = len(riddles)
-    
+
     if request.method == "GET":
-        
+
         # print("I am here in the 'if GET' statement")
         return render_template("quiz.html", coffee_question=coffee_question,
                                             coffee_image=coffee_image,
@@ -112,79 +112,145 @@ def get_coffee_quiz():
     #Trying to pass the answer and check if correct - START
     elif request.method == "POST":
         if request.form:
-            print('request.form is:', request.form)
-            #The guess would equal the user's input
-            coffee_guess = request.form["answer"].lower()
-            mistake = False
-            print("coffee_answer: ", coffee_answer, "coffee_guess:  ", coffee_guess)
-            print("session num:", session['quiz_num'])
-            #We need to check that our answer is correct
-            if coffee_guess == coffee_answer:
-                if session['quiz_num'] < len(riddles) - 1:
-                    #If it is correct, we add 1 to our score and print some feedback
-                    print("right!")
-                    #Add points to "score"
-                    session['score'] += 1
-                    session['quiz_num'] += 1
-                    
-                    #Now we need to get the next question and its answer, 
-                    #remember the session has increased by one
-                    coffee_question = get_coffee_questions(riddles, session['quiz_num'])
-                    coffee_image = get_coffee_images(riddles, session['quiz_num'])
-                    coffee_answer = get_coffee_answers(riddles, session['quiz_num']).lower()
-                    
-                    print("session['quiz_num'] is: ", session['quiz_num'])
-                    
-                    #PENDING - Get the Flash to work
-                    flash("Correct!")
-                    return render_template("quiz.html", username=session["username"], 
+            flag = request.form.keys()
+            if request.form.keys()[0] == 'answer':
+                print('The request was answer')
+                # print('request.form is:', request.form)
+                #The guess would equal the user's input
+                coffee_guess = request.form['answer'].lower()
+                mistake = False
+                # print("coffee_answer: ", coffee_answer, "coffee_guess:  ", coffee_guess)
+                # print("session num:", session['quiz_num'])
+                #We need to check that our answer is correct
+                if coffee_guess == coffee_answer:
+                    if session['quiz_num'] < len(riddles) - 1:
+                        #If it is correct, we add 1 to our score and print some feedback
+                        print("right!")
+                        #Add points to "score"
+                        session['score'] += 1
+                        session['quiz_num'] += 1
+    
+                        #Now we need to get the next question and its answer,
+                        #remember the session has increased by one
+                        coffee_question = get_coffee_questions(riddles, session['quiz_num'])
+                        coffee_image = get_coffee_images(riddles, session['quiz_num'])
+                        coffee_answer = get_coffee_answers(riddles, session['quiz_num']).lower()
+    
+                        print("session['quiz_num'] is: ", session['quiz_num'])
+    
+                        #PENDING - Get the Flash to work
+                        flash("Correct!")
+                        return render_template("quiz.html", username=session["username"],
+                                                    coffee_question=coffee_question,
+                                                    coffee_image=coffee_image,
+                                                    coffee_answer=coffee_answer,
+                                                    coffee_guess=coffee_guess,
+                                                    score=session["score"],
+                                                    session=session["quiz_num"],
+                                                    number_of_questions=number_of_questions,
+                                                    mistake=mistake)
+    
+                    elif session['quiz_num'] == len(riddles) - 1:
+                        #If it is correct, we add 1 to our score and print some feedback
+                        print("right!")
+                        #Add points to "score"
+                        session['score'] += 1
+                        ## Because we don't want to be out of range, we don't increment session
+                        # session['quiz_num'] += 0
+    
+                        # We get the last question and its answer
+                        coffee_question = get_coffee_questions(riddles, session['quiz_num'])
+                        coffee_image = get_coffee_images(riddles, session['quiz_num'])
+                        coffee_answer = get_coffee_answers(riddles, session['quiz_num']).lower()
+                        print("Last question, we now go to game over page")
+    
+                        #PENDING - Get the Flash to work
+                        flash("Correct!")
+                        return redirect(url_for('gameover'))
+    
+                else:
+                    print("wrong!")
+                    print("coffee_guess: ", coffee_guess)
+                    mistake = True
+                    flash("Wrong!")
+                    return render_template("quiz.html", username=session["username"],
                                                 coffee_question=coffee_question,
                                                 coffee_image=coffee_image,
                                                 coffee_answer=coffee_answer,
                                                 coffee_guess=coffee_guess,
-                                                score=session["score"], 
+                                                score=session["score"],
                                                 session=session["quiz_num"],
                                                 number_of_questions=number_of_questions,
                                                 mistake=mistake)
-                
-                elif session['quiz_num'] == len(riddles) - 1:
-                    #If it is correct, we add 1 to our score and print some feedback
-                    print("right!")
-                    #Add points to "score"
-                    session['score'] += 1
-                    ## Because we don't want to be out of range, we don't increment session
-                    # session['quiz_num'] += 0
-                    
-                    # We get the last question and its answer
-                    coffee_question = get_coffee_questions(riddles, session['quiz_num'])
-                    coffee_image = get_coffee_images(riddles, session['quiz_num'])
-                    coffee_answer = get_coffee_answers(riddles, session['quiz_num']).lower()
-                    print("Last question, we now go to game over page")
-                    
-                    #PENDING - Get the Flash to work
-                    flash("Correct!")
-                    return redirect(url_for('gameover'))
 
-            else:
-                print("wrong!")
-                print("coffee_guess: ", coffee_guess)
-                mistake = True
-                flash("Wrong!")
-                return render_template("quiz.html", username=session["username"], 
-                                            coffee_question=coffee_question,
-                                            coffee_image=coffee_image,
-                                            coffee_answer=coffee_answer,
-                                            coffee_guess=coffee_guess,
-                                            score=session["score"],
-                                            session=session["quiz_num"],
-                                            number_of_questions=number_of_questions,
-                                            mistake=mistake)
-                
         #Trying to pass the answer and check if correct - END
+            if request.form.keys()[0] == 'solution':
+                coffee_guess = request.form['solution'].lower()
+                mistake = False
+                if coffee_guess == coffee_answer:
+                    if session['quiz_num'] < len(riddles) - 1:
+                        #If it is correct, we add 1 to our score and print some feedback
+                        print("right!")
+                        #Add points to "score"
+                        session['score'] += 0
+                        session['quiz_num'] += 1
     
+                        #Now we need to get the next question and its answer,
+                        #remember the session has increased by one
+                        coffee_question = get_coffee_questions(riddles, session['quiz_num'])
+                        coffee_image = get_coffee_images(riddles, session['quiz_num'])
+                        coffee_answer = get_coffee_answers(riddles, session['quiz_num']).lower()
     
-# PENDING - Changes need to be made as the program runs when testing which is 
-#unwanted. 
+                        print("session['quiz_num'] is: ", session['quiz_num'])
+    
+                        #PENDING - Get the Flash to work
+                        flash("Correct!")
+                        return render_template("quiz.html", username=session["username"],
+                                                    coffee_question=coffee_question,
+                                                    coffee_image=coffee_image,
+                                                    coffee_answer=coffee_answer,
+                                                    coffee_guess=coffee_guess,
+                                                    score=session["score"],
+                                                    session=session["quiz_num"],
+                                                    number_of_questions=number_of_questions,
+                                                    mistake=mistake)
+    
+                    elif session['quiz_num'] == len(riddles) - 1:
+                        #If it is correct, we add 1 to our score and print some feedback
+                        print("right!")
+                        #Add points to "score"
+                        session['score'] += 0
+                        ## Because we don't want to be out of range, we don't increment session
+                        # session['quiz_num'] += 0
+    
+                        # We get the last question and its answer
+                        coffee_question = get_coffee_questions(riddles, session['quiz_num'])
+                        coffee_image = get_coffee_images(riddles, session['quiz_num'])
+                        coffee_answer = get_coffee_answers(riddles, session['quiz_num']).lower()
+                        print("Last question, we now go to game over page")
+    
+                        #PENDING - Get the Flash to work
+                        flash("Correct!")
+                        return redirect(url_for('gameover'))
+    
+                else:
+                    print("wrong!")
+                    print("coffee_guess: ", coffee_guess)
+                    mistake = True
+                    flash("Wrong!")
+                    return render_template("quiz.html", username=session["username"],
+                                                coffee_question=coffee_question,
+                                                coffee_image=coffee_image,
+                                                coffee_answer=coffee_answer,
+                                                coffee_guess=coffee_guess,
+                                                score=session["score"],
+                                                session=session["quiz_num"],
+                                                number_of_questions=number_of_questions,
+                                                mistake=mistake)
+
+
+# PENDING - Changes need to be made as the program runs when testing which is
+#unwanted.
 
 @app.route('/gameover')
 def gameover():
--- static/quiz_loop.js
+++ static/quiz_loop.js
@@ -28,3 +28,24 @@
         
 //     });
 // </script>
+
+
+/* to handle form posting on button click:
+
+1. preventDefault() - prevent the form from being POSTed
+2. Remove action="/quiz" from <form> element
+3. Check whether mistake is true
+    if true:
+        Pop up modal
+    else:
+        POST to quiz function w/ javascript
+        $.post({
+            data: {
+                'guess': what's in the form,
+                'url': '/quiz'
+            }
+        }).done(function(data){
+           Handle what to do when the results come back 
+           score.innerHTML(score)
+        });
+*/
